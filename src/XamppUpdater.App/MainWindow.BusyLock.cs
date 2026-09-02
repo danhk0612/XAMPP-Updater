3ce6d89c7d7231eac8470f745f7af89a328bd986
HAEKWANG DAN <danhk0612@gmail.com>
2026-09-02 15:17:52 +0900

    fix: block concurrent UI actions while busy

---
+++ src/XamppUpdater.App/MainWindow.BusyLock.cs
@@ -0,0 +1,23 @@
+using System.Windows;
+
+namespace XamppUpdater.App;
+
+public partial class MainWindow
+{
+    protected override void OnContentRendered(EventArgs e)
+    {
+        base.OnContentRendered(e);
+        OnlineCheckButton.IsEnabledChanged += OnlineCheckButton_IsEnabledChanged;
+        SyncWindowInputLock();
+    }
+
+    private void OnlineCheckButton_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        SyncWindowInputLock();
+    }
+
+    private void SyncWindowInputLock()
+    {
+        RootGrid.IsHitTestVisible = OnlineCheckButton.IsEnabled;
+    }
+}
