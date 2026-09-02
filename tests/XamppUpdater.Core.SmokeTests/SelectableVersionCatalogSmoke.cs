d9b8b19cacdda067029e4dd090ba7a4d3fd0ae08
HAEKWANG DAN <danhk0612@gmail.com>
2026-09-02 14:20:14 +0900

    test: cover latest-per-series version filtering

--- tests/XamppUpdater.Core.SmokeTests/SelectableVersionCatalogSmoke.cs
+++ tests/XamppUpdater.Core.SmokeTests/SelectableVersionCatalogSmoke.cs
@@ -8,21 +8,28 @@ internal static class SelectableVersionCatalogSmoke
     internal static void Run()
     {
         var apache = SelectableVersionCatalogService.ParseApacheArchiveVersions(
-            "CHANGES_2.4.41 httpd-2.4.62.tar.bz2 httpd-2.4.68.tar.bz2",
-            "2.4.41");
-        AssertContains("Apache selectable 2.4.62", apache.Select(item => item.Version), "2.4.62");
-        AssertContains("Apache selectable 2.4.68", apache.Select(item => item.Version), "2.4.68");
+            "httpd-2.2.34.tar.bz2 httpd-2.2.35.tar.bz2 httpd-2.4.62.tar.bz2 httpd-2.4.68.tar.bz2",
+            "2.2.10");
+        AssertContains("Apache 2.2.x latest", apache.Select(item => item.Version), "2.2.35");
+        AssertContains("Apache 2.4.x latest", apache.Select(item => item.Version), "2.4.68");
+        AssertNotContains("Apache older patch filtered", apache.Select(item => item.Version), "2.4.62");
 
         var php = SelectableVersionCatalogService.ParsePhpArchiveVersions(
             "php-7.3.33-Win32-VC15-x64.zip " +
+            "php-8.3.24-Win32-vs16-x64.zip " +
+            "php-8.3.27-Win32-vs16-x64.zip " +
             "php-8.4.24-Win32-vs17-x64.zip " +
+            "php-8.4.28-Win32-vs17-x64.zip " +
             "php-8.5.10-Win32-vs17-x64.zip " +
             "php-8.5.10-nts-Win32-vs17-x64.zip",
             "7.3.11",
             BinaryArchitecture.X64,
             requireThreadSafe: true);
-        AssertContains("PHP selectable 7.3.33", php.Select(item => item.Version), "7.3.33");
-        AssertContains("PHP selectable 8.5.10", php.Select(item => item.Version), "8.5.10");
+        AssertContains("PHP 7.3.x latest", php.Select(item => item.Version), "7.3.33");
+        AssertContains("PHP 8.3.x latest", php.Select(item => item.Version), "8.3.27");
+        AssertContains("PHP 8.4.x latest", php.Select(item => item.Version), "8.4.28");
+        AssertContains("PHP 8.5.x latest", php.Select(item => item.Version), "8.5.10");
+        AssertNotContains("PHP older patch filtered", php.Select(item => item.Version), "8.3.24");
         var phpLatest = php.First(item => item.Version == "8.5.10");
         AssertEqual("PHP latest package", "php-8.5.10-Win32-vs17-x64.zip", phpLatest.PackageFileName);
 
@@ -37,6 +44,7 @@ internal static class SelectableVersionCatalogSmoke
             new SelectableVersionCatalogService.MariaDbSeriesEntry("10.4", true),
             "10.4.8",
             BinaryArchitecture.X64);
+        AssertEqual("MariaDB one latest per series", "1", mariaDb.Count.ToString());
         AssertContains("MariaDB selectable 10.4.34", mariaDb.Select(item => item.Version), "10.4.34");
     }
 
@@ -45,6 +53,14 @@ internal static class SelectableVersionCatalogSmoke
         if (!values.Contains(expected, StringComparer.OrdinalIgnoreCase))
         {
             throw new InvalidOperationException($"{name}: expected '{expected}'.");
+        }
+    }
+
+    private static void AssertNotContains(string name, IEnumerable<string> values, string unexpected)
+    {
+        if (values.Contains(unexpected, StringComparer.OrdinalIgnoreCase))
+        {
+            throw new InvalidOperationException($"{name}: unexpected '{unexpected}'.");
         }
     }
 
