9cdb48f968fe797d31916e93fe1e9b7c32f540cd
HAEKWANG DAN <danhk0612@gmail.com>
2026-09-03 10:58:29 +0900

    Show build timestamp in window title

--- src/XamppUpdater.App/App.xaml.cs
+++ src/XamppUpdater.App/App.xaml.cs
@@ -1,7 +1,29 @@
+using System.Reflection;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace XamppUpdater.App;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, () =>
+        {
+            if (MainWindow is not Window window) return;
+
+            try
+            {
+                var assemblyPath = Assembly.GetExecutingAssembly().Location;
+                var buildTime = File.GetLastWriteTime(assemblyPath);
+                window.Title = $"XAMPP Updater - Build {buildTime:yyyy-MM-dd HH:mm:ss}";
+            }
+            catch
+            {
+                window.Title = "XAMPP Updater - Build unknown";
+            }
+        });
+    }
 }
